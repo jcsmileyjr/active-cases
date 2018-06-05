6614910f848c25099f565cfc788f2c610881e5d6
JC Smiley <jcsmileyjr@gmail.com>
2018-06-04 19:08:50 -0500

    add icon and  CSS to add new case component
    - linked in the fontawesome icon CDN
    - made the new icon and div's text bigger and stand out.

--- src/Components/AddCaseButton/addCaseButton.js
+++ src/Components/AddCaseButton/addCaseButton.js
@@ -7,9 +7,9 @@ class AddCaseButton extends Component{
     return(
       <div className="row clickNewButton">
         <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-          <i class="fas fa-plus-circle"></i>
+          <i class="fas fa-plus fa-2x"></i>
         </div>
-        <div className="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+        <div className="col-xs-9 col-sm-9 col-md-9 col-lg-9 buttenTextSize">
           Click to Add New Case
         </div>      
       </div>
