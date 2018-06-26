c88fa62dc0e071a2211a83d30bf7c5f873220410
JC Smiley <jcsmileyjr@gmail.com>
2018-06-26 06:57:02 -0500

    Redesign case's title placement
    Problem: On mobile screens the case title is not showing what the data
    is. The solution is to move the title from under the title to besides
    the data its reflecting.

--- src/Components/Case/Case.js
+++ src/Components/Case/Case.js
@@ -16,19 +16,46 @@ class Case extends Component{
 	render(){
 		return (
 			<div className="row casesContainer centerCaseInfo" data-toggle="tooltip" data-placement="top" title={"Days remaining til due: " + (this.props.daysLeft + 1)} onClick={this.openChangeStatusClick}>
-				<div className="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+				<div className="hidden-xs col-sm-3 col-md-3 col-lg-3">
 					{this.props.type}
 				</div>
-				<div className="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+				<div className="hidden-xs col-sm-3 col-md-3 col-lg-3">
 					{this.props.casino}
 				</div>
-				<div className="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+				<div className="hidden-xs col-sm-3 col-md-3 col-lg-3">
 					{this.props.patron}
 				</div>
-				<div className="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+				<div className="hidden-xs col-sm-3 col-md-3 col-lg-3">
+					{this.props.status}
+				</div>
+
+
+				<div className="col-xs-3 hidden-sm hidden-md hidden-lg">
+					<strong>Type:</strong>
+				</div>
+				<div className="col-xs-9 hidden-sm hidden-md hidden-lg">
+					{this.props.type}
+				</div>
+				<div className="col-xs-3 hidden-sm hidden-md hidden-lg">
+					<strong>Casino:</strong>
+				</div>
+				<div className="col-xs-9 hidden-sm hidden-md hidden-lg">
+					{this.props.casino}
+				</div>
+				<div className="col-xs-3 hidden-sm hidden-md hidden-lg">
+					<strong>Patron:</strong>
+				</div>
+				<div className="col-xs-9 hidden-sm hidden-md hidden-lg">
+					{this.props.patron}
+				</div>
+				<div className="col-xs-3 hidden-sm hidden-md hidden-lg">
+					<strong>Status:</strong>
+				</div>
+				<div className="col-xs-9 hidden-sm hidden-md hidden-lg">
 					{this.props.status}
 				</div>
 			</div>
+
 
 		);	  	
 	}
