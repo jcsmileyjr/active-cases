9aae500b4ac1aff84a061b782769394cb07bf3d8
JC Smiley <jcsmileyjr@gmail.com>
2018-06-26 07:12:38 -0500

    Progression list data not lined up on mobile view
    Problem: While in mobile view, the progression status and days required
    wasn't lined up. It looked disorganized.

--- src/Components/ProgressionList/progressionList.js
+++ src/Components/ProgressionList/progressionList.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import '../Nav/nav.css';
+import './progressionList.css';
 
 function ProgressionList(props){
   return(
     <div className="row instructionsContainer">
-      <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+      <div className="col-xs-7 col-sm-8 col-md-8 col-lg-8">
         <h4>Progression Key</h4>
       </div>
-      <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+      <div className="col-xs-5 col-sm-4 col-md-4 col-lg-4 rightAlign">
         <h4># of Days</h4>
       </div>
-      <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+      <div className="col-xs-10 col-sm-8 col-md-8 col-lg-8">
         <p>Waiting on letter from patron</p>
         <p>Active</p>
         <p>On supervisor's desk</p>
@@ -20,14 +20,14 @@ function ProgressionList(props){
         <p>Sign and close out</p>
         <p>To be file</p>
       </div>
-      <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-        <p>30 days</p>
-        <p>10 days</p>
-        <p>3 days</p>
-        <p>3 days</p>
-        <p>10 days</p>
-        <p>30 days</p>
-        <p>3 days</p>
+      <div className="col-xs-2 col-sm-4 col-md-4 col-lg-4">
+        <p>30 </p>
+        <p>10 </p>
+        <p>3 </p>
+        <p>3 </p>
+        <p>10 </p>
+        <p>30 </p>
+        <p>3 </p>
         <p>Delete</p>
       </div>          
     </div>  
