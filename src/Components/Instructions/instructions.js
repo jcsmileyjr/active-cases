da5c0ab5f83843794d93857d3258a409882640de
JC Smiley <jcsmileyjr@gmail.com>
2018-06-29 20:39:48 -0500

    Updated Instructions  to describe Judicial Process
    Stated clearly what was included in this new status

--- src/Components/Instructions/instructions.js
+++ src/Components/Instructions/instructions.js
@@ -6,9 +6,10 @@ function Instructions(props){
 	<div className="instructionsContainer">
 	  <h3 className="centerAlign"> Instructions</h3>
       <ul className="listOfInstructions">
-        <li>The color of the status for each case willl change based on days remaining for a task to be done</li>
+        <li>The color of the status for each case will change based on days remaining for a task to be done</li>
         <li>Click the plus icon to create a new case</li>
         <li>Click on a case to <span className="blueStatusColor"> update the status</span></li>
+	  	<li>Judicial Process includes crimminal, minors, revocation, and disputes going to court</li>
       </ul>
 	</div>  
   );
